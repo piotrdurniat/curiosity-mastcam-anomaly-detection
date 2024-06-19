13e74661c4ee2a4e3dd0162d083741196dde0a6f
applesoju <patryknmarciniak@gmail.com>
2024-06-19 12:21:44 +0200

    corrected datatype of one argument

--- flow/mask.py
+++ flow/mask.py
@@ -7,7 +7,7 @@ class CheckerboardMask():
         width: int,
         dtype: torch.dtype = torch.float32,
         requires_grad: bool = False,
-        device: str = None,
+        device: torch.device = None,
     ):
         checkerboard = [[((i % 2) + j) % 2 for j in range(width)] for i in range(height)]
         self.mask = torch.tensor(checkerboard, dtype=dtype, device=device, requires_grad=requires_grad)
