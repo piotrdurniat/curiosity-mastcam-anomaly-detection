d35d41c246e1759f9490022e7ad9d06f5d239011
applesoju <patryknmarciniak@gmail.com>
2024-06-19 11:41:14 +0200

    initial commit

---
+++ flow/coupling_layer.py
@@ -0,0 +1,5 @@
+import torch
+import torch.nn as nn
+
+class CouplingLayer(nn.Module):
+    raise NotImplementedError
---
+++ flow/real_nvp.py
@@ -0,0 +1,5 @@
+import torch
+import torch.nn as nn
+
+class Real_NVP(nn.Module):
+    raise NotImplementedError
