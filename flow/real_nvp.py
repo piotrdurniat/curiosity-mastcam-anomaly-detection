a97e4d09897e2b37ed7d9c7dd311345bee9f5982
applesoju <patryknmarciniak@gmail.com>
2024-06-19 17:01:08 +0200

    moved mask getter into real_nvp class

--- flow/real_nvp.py
+++ flow/real_nvp.py
@@ -1,5 +1,20 @@
 import torch
 import torch.nn as nn
 
-class Real_NVP(nn.Module):
-    raise NotImplementedError
+class RealNVP(nn.Module):
+    def __init__(self):
+        raise NotImplementedError
+    
+
+    def _get_mask(
+        height: int,
+        width: int,
+        channels: int,
+        dtype: torch.dtype = torch.float32,
+        requires_grad: bool = False,
+        device: torch.device = None,
+    ):
+        checkerboard = [[((i % 2) + j) % 2 for j in range(width)] for i in range(height)]
+        mask = torch.tensor(checkerboard, dtype=dtype, device=device, requires_grad=requires_grad)
+
+        return mask.unsqueeze(0).repeat(channels, 1, 1)
