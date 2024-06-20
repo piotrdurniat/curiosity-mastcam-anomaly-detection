4207118748bd18dddd238a7ea51bde67fa599c2c
applesoju <patryknmarciniak@gmail.com>
2024-06-20 12:26:11 +0200

    changed some prints

--- flow/train_flow.py
+++ flow/train_flow.py
@@ -1,11 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import time
 
 from .real_nvp import RealNVP
 
 from torch import Tensor
 from torch.utils.data import DataLoader
+
 
 
 class TrainerRealNVP:
@@ -30,6 +32,7 @@ class TrainerRealNVP:
 
         for epoch in range(self.epochs):
             print(f"Starting epoch {epoch + 1}")
+            start_time = time.time()
 
             epoch_loss = 0.0
             for image, _ in self.train_loader:
@@ -50,7 +53,9 @@ class TrainerRealNVP:
                 epoch_loss += nll.item()
                 print(nll.item())
 
-            print(f"Training... Epoch: {epoch+1}, Loss: {epoch_loss / len(self.train_loader):.3f}")
+            epoch_time = time.time() - start_time()
+            print(f"Epoch: {epoch+1} done in {epoch_time:.2f} seconds")
+            print(f"Loss: {epoch_loss / len(self.train_loader):.3f}")
 
 
     def _loss(self, y: Tensor, log_det: Tensor):
