01f0b2a4cdbb65374fd348ea2c26d7da49a2dbb5
applesoju <patryknmarciniak@gmail.com>
2024-06-22 17:03:32 +0200

    adjusted utilis.py

--- utilis.py
+++ utilis.py
@@ -3,15 +3,14 @@ import torch.nn as nn
 import numpy as np
 
 import torchvision.transforms as transforms
-import flow
-import flow.coupling_layer
-import flow.real_nvp
 import flow.train_flow
+import flow.maf
 import dataset
 import yaml
 
 from torch import Tensor
 from torch.utils.data import DataLoader
+import torch.optim as optim
 
 
 PATH_TRAIN  = './dataset/train_typical'
@@ -27,7 +26,7 @@ FREQ_PRINT = 20
 
 def train_model(model_name, epoch_number, lr, device):
 
-    transform = dataset.ToTensorWithScaling()
+    transform = dataset.Dequantize()
 
     print(model_name, lr, epoch_number, device)
 
@@ -47,51 +46,26 @@ def train_model(model_name, epoch_number, lr, device):
 
     if model_name == "GAN":
         pass
-        # model = BiGAN.train_GAN.TrainerBiGAN(epoch_number, lr, train_loader, device)
-        # encoder, generator, discriminator = model.train()
-
-        # torch.save({
-        #     'encoder_state_dict': encoder.state_dict(),
-        #     'generator_state_dict': generator.state_dict(),
-        #     'discriminator_state_dict': discriminator.state_dict(),
-        # }, 'models/models.pth')
-
-
-        ### Sekcja do testów anaomali 
         
-        # print("LOADING")
-
-        # encoder = BiGAN.encoder.GanEncoder().to(device)  # Replace Encoder with your actual encoder class
-        # generator = BiGAN.generator.GanGenerator().to(device) # Replace Generator with your actual generator class
-        # discriminator = BiGAN.discriminator.GanDiscriminator().to(device)  # Replace Discriminator with your actual discriminator class
-
-        # checkpoint = torch.load('models/models2.pth')
-        # encoder.load_state_dict(checkpoint['encoder_state_dict'])
-        # generator.load_state_dict(checkpoint['generator_state_dict'])
-        # discriminator.load_state_dict(checkpoint['discriminator_state_dict'])
-        
-        # print("#################### NORMAL ####################")
-
-        # tester = BiGAN.detect_GAN.AnomalyScore(generator, encoder, discriminator, test_typical_loader, device)
-        # result = tester.test()
-
-        # print("#################### NOVEL ####################")
-
-        # tester = BiGAN.detect_GAN.AnomalyScore(generator, encoder, discriminator, test_novel_loader, device)
-        # result = tester.test()
     elif model_name == "VAE":
         pass
 
     elif model_name == "FLOW":
-        model = flow.real_nvp.RealNVP(6, 64, 6)
-        trainer = flow.train_flow.TrainerRealNVP(model, epoch_number, lr, train_loader, device)
-        trainer.train()
 
-        torch.save({
-            'model_state_dict': model.state_dict(),
-        }, 'models/flow.pth')
+        model = flow.maf.MAF(6 * 64 * 64, [64], 5, use_reverse=True)
+        # trainer = flow.train_flow.TrainerMAF(model, epoch_number, lr, train_loader, device)
+        # trainer.train()
+
+        # torch.save({
+        #     'model_state_dict': model.state_dict(),
+        # }, 'models/maf_02.pth')
+
+        checkpoint = torch.load('models/maf_02.pth')
+        model.load_state_dict(checkpoint["model_state_dict"])
+
+        print("ok")
 
     else:
         raise ValueError("Unkown Model")
     
-# train_model(model_name="FLOW", epoch_number=2, lr=1e-3, device="cpu")
+train_model(model_name="FLOW", epoch_number=2, lr=1e-4, device="cpu")
